28511c19dca2f5ff84d44df79c8d1ef34cb99c69
황채원 <wondreaming@naver.com>
2024-03-12 17:50:49 +0900

    [Feat] #146 AppBar #front
    Former-commit-id: 1ccc2aa8043b56a95c7a44b7cd1e71edd0624cc9

--- front/app/lib/main.dart
+++ front/app/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88ce0d7b3d68a408c64cc488a003cf38a6a6ac66aaae286a7f773c5c435f7773
-size 3483
+oid sha256:325dfbf5a01a42d851c33ed369056ccac5c346eb3e83f159989a4d65f6b2ef2a
+size 9101
---
+++ front/app/lib/style.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+var Theme = ThemeData();
