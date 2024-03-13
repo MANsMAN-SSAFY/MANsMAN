8981777c8aef47d24a71bf8f3f5ee8c682fe02a1
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-03-13 17:58:47 +0900

    [Feat] #216 React페이지 초기 구성
    Former-commit-id: 834d0cdcf9485336fe88679de3962633c1bfa012

---
+++ front/front/src/components/ArticleList.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aeac4a22533a0e5587e74d2ea6fb33ac76d9fa0c4141f856967d4c1dcf2f74a8
+size 645
---
+++ front/front/src/pages/RecordPage.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+export default function RecordPage() {
+  return (
+    <>
+      <h1>일지 페이지</h1>
+    </>
+  );
+}
