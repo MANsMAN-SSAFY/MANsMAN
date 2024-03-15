9c495d6a8713ef45b71d65ae96d6f54330684a35
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-03-15 17:05:52 +0900

    [Feat] #172 게시판 댓글
    게시판 댓글 작성칸, 아이콘, 작성 완료
    
    
    Former-commit-id: f99f9f376f781a3a61f60e1e209e64e553a694f4

---
+++ front/front/src/pages/profile/MyArticlePage.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+import MyArticleList from "../../components/profile/MyArticleList.jsx";
+
+export default function MyArticlePage() {
+  return (
+    <>
+      <MyArticleList />
+    </>
+  );
+}
