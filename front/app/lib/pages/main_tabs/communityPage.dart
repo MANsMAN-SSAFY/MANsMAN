f0869182ba670cef3d0175487807f4586995374a
황채원 <wondreaming@naver.com>
2024-03-15 17:12:27 +0900

    [Feat] #170 회원가입3 #front
    Former-commit-id: a0889b6e63c97d768309cbeb25dad1e08e7cccd4

--- front/app/lib/pages/main_tabs/communityPage.dart
+++ front/app/lib/pages/main_tabs/communityPage.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dba98541a49830c04bbb81bb4eb9026300d9358e905803a97e4f53097b16ac98
-size 2249
+oid sha256:5b4d99f78c33dcf6fabb3010dff257e8d21ce135e4250b6928a6097651439038
+size 2030
--- front/app/lib/styles/app_colors.dart
+++ front/app/lib/styles/app_colors.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 class AppColors{
   static const blue = Color(0xff0466C8);
   static const background = Color(0xffFEFCFB);
+  static const tag = Color(0xffD9D9D9);
 }
