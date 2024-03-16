91d9e74f9a58d7c1c01a07d6acde5a6a7f59e976
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-03-16 23:24:17 +0900

    [Feat] #172 게시판 내가쓴글 삭제
    내가쓴글 삭제
    글쓰기 에디터 수정 중
    
    
    Former-commit-id: 440feade03090fe711704f472579639d1448611d

--- front/front/src/components/articles/ArticleDetail.jsx
+++ front/front/src/components/articles/ArticleDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d198790bbf53e471c065d5fc6102729a217cbfc2f6ca2a770731fd2efabe5aa
-size 3425
+oid sha256:0aa31a3f7971cc110e753bc4872daabe00328ee96db1b92750120e3c8cbfba11
+size 3967
---
+++ front/front/src/components/articles/ArticleEditor.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0d9a0319ff461f27653c6a50ff6d2276b8208e8570686a755647cf7ef11ae09
+size 3089
