2ed0d427915d09d7906e1b333055a19c5da8372d
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-03-18 12:44:10 +0900

    [Feat] #173 게시글 뒤로가기
    Former-commit-id: 331d7c77f20947ec74a335b7ad867b85edc57765

--- front/src/components/articles/ArticleDetail.jsx
+++ front/src/components/articles/ArticleDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0aa31a3f7971cc110e753bc4872daabe00328ee96db1b92750120e3c8cbfba11
-size 3967
+oid sha256:e87fc67d7bc994c50d14546a2603799e5a44b00dc74769909406d3155cb5609d
+size 4254
