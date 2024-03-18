8be9259bc0ef0ea5904313fdfb5e40f1babd69e9
황채원 <wondreaming@naver.com>
2024-03-18 17:33:54 +0900

    [Design] #220 회원가입UI #front
    Former-commit-id: 70d4588bca7dc296ef6da07d45d354f9ffbfa444

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34e9f0a31970d375908ba47843790e0ee14effa169f75aa461aa5cb0f4931b2e
-size 1412
+oid sha256:ecc588605d24fd28d9042348b3f2a52240d3154f081a6ec070a775326fd61c23
+size 1526
