b8438030b3ccf82badd7971be4647a75c0cda5a0
김동영 <ehddud0534@naver.com>
2024-03-18 23:38:41 +0900

    [Feat] #224 피부일지 캐러셀 인디케이터
    Former-commit-id: 79322e75e1a624bcd1990daa24ee4b23988a25e0

--- app/lib/components/skin_picture.dart
+++ app/lib/components/skin_picture.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bba99cab96e8410e44f7fa76ce2ee54096783541559ac2f0d534be7eed18ed2e
-size 2717
+oid sha256:bfc8561499ceffe56cd4e85f2ab66796481a6731a47f14f162eca941a7a120dd
+size 2758
--- app/lib/pages/board_detail_page.dart
+++ app/lib/pages/board_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08acbece3f6321414729bd660c0753b684e93409a62c080049f786dae21fdd6d
-size 1818
+oid sha256:3f9a44171d589eb7e699e5bd5fb035c4d4899b9c3dc4b5511ff85ab75b77a387
+size 1868
