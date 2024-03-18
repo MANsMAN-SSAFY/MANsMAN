cd3fbf8ebe7dc7a13c4fe4e4c1a007395f4f05f5
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-03-18 09:19:22 +0900

    [Fix] #172 게시판 수정
    Former-commit-id: 0b576bd2327e648d398d03057fa04cc73a18c256

--- front/front/src/pages/community/CommunityPage.jsx
+++ front/front/src/pages/community/CommunityPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf4b481d7d9b6ca1eaf79c91d93f3326df422e07cecdd88c57f69822c71bbfd8
-size 716
+oid sha256:d0b8d6208658c500a9e55a7160b72497a8ac2d08cf12c2fe618463610b8503c6
+size 654
