96eac322557b980ba0bbe6d03823ebad517440a9
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-03-18 11:14:49 +0900

    [Refactor] #218 프론트 폴더 리팩토링
    Former-commit-id: 05567b935d00add6e48df9f28b5aa749573e7aa4

--- front/MsM/lib/components/product.dart
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:484564292835360b00a90e50ae00d3ba538fdbe7594f3bbd3c93e2fe6c938f7c
-size 838
--- front/MsM/lib/config/app_routes.dart
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:65c5518d26e11563b4ac691f33e4cf061d2b9f1dda32fec9f2d9c409a595aa66
-size 1161
