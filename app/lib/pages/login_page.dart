c57317245fd83bc67bcd0a6074d70106925fd817
황채원 <wondreaming@naver.com>
2024-03-19 15:52:57 +0900

    [Fix] #258 로그인 유효성 검사2
    Former-commit-id: 60449b43ccc0a816c97af9254b22a6cbcfe4e696

--- app/lib/pages/login_page.dart
+++ app/lib/pages/login_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe9c6a794cf98207b628e18daf7a6dff26763dece6d6926829c321f6aa362e59
-size 6940
+oid sha256:b831af7600eccd0cbaf91c2017332f73e7c30cdadfede7d668c22168f1412b3c
+size 7309
