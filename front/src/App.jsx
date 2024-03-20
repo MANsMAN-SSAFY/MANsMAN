dd150dbc74e18965497c6894f39fbb1fcdb39893
HYEONMIN LEE <hyunmin2667@gmail.com>
2024-03-20 23:11:50 +0900

    [Chore] #228 front CICD 개발모드 환경 구성 fix7
    Former-commit-id: 4c11dbe0f23c815be8aa98f0ff04552450e724d8

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b18a9f28f93efea4124e614dbbd11173eafb012d11563dbf1377e899aa544b54
-size 1738
+oid sha256:cb01505d3cee9efb8f59c5c6e7f3badca0165220071aae45916b0ac2421d3936
+size 1741
