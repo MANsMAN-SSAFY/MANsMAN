57eebb460d69135a77d98fdd73ba648544383c62
송승준 <ssj0187@naver.com>
2024-03-20 15:07:26 +0900

    [Feat] 닉네임 추가 및 수정 로직 구현
    Former-commit-id: 79c5616e9ca6297e94f01a2c57c6cce6b7daa7e9

---
+++ back/src/main/java/com/msm/back/member/dto/NicknameUpdateResponseDto.java
@@ -0,0 +1,12 @@
+package com.msm.back.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class NicknameUpdateResponseDto {
+    private String nickname;
+}
--- back/src/main/java/com/msm/back/member/service/MemberService.java
+++ back/src/main/java/com/msm/back/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d75ae79891ef103d64d0be6f3c985280ce419c7572484b0ca8c5901a14b0e12
-size 3537
+oid sha256:354cd0fcc524a6ffb2497590744edf960b89c3504e444b2f6417dd18e4348b68
+size 4149
