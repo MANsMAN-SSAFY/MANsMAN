ef0a5ab42650bbf92bfae038234cfc25e2ddb478
송승준 <ssj0187@naver.com>
2024-03-21 17:59:36 +0900

    [Feat] #271 스크랩 리스트 조회 기능 구현
    Former-commit-id: e90e779d746567e6f7e3a9af9ad61d5c7b597cc8

---
+++ back/src/main/java/com/msm/back/board/entity/Scrap.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:57eedad91f2bb1962b0868f099521d6f0c33132cb7e824acbfe82444fd1f787c
+size 593
--- back/src/main/java/com/msm/back/member/service/MemberService.java
+++ back/src/main/java/com/msm/back/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a562fad8c0236aa48d8610a0b5b03bbb3a98f07e6e11342fd664a4f41ed6054
-size 7943
+oid sha256:05c147747436d93a15a4aa814fa0e758101cf76670c0f52f0b6b0644424416bd
+size 8539
