81f190877433e07cf9eb4a77394c2cb8459b3694
송승준 <ssj0187@naver.com>
2024-03-21 11:30:33 +0900

    [Refactor] Entity Column name 수정
    Former-commit-id: ff98db87b59a870494e657be6403eb3d0ecdf4ae

--- back/src/main/java/com/msm/back/common/BaseEntity.java
+++ back/src/main/java/com/msm/back/common/BaseEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8a2e701ee1564d5f2e461e04d9b18ca317deaae04bd445d463ff012e21dbe62
-size 708
+oid sha256:064660c7cb4f467c34cd8320c2f8ab9eaec48be749dbdcc93add5b021f0e5128
+size 654
