5c78ecc4c397ac5891a9e7c4cc7fa71f58b95fc1
김동영 <ehddud0534@naver.com>
2024-03-21 12:08:38 +0900

    [Feat] #230 일지 Provider 상태 연결
    차트 재랜더링
    
    
    Former-commit-id: 0d5a49dd995ffe40317c4264e4496ec9aa70bc07

--- app/lib/components/skin_picture.dart
+++ app/lib/components/skin_picture.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c0ff62d3252e78c634c49abc142b95256f5ebc2daa34de7a432741015bc2e99
-size 5565
+oid sha256:5dd725a07d82ebcd40f7661a4d4906470c3a9d2e799bf31717afdded95a05c1a
+size 6373
