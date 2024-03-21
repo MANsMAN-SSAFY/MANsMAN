be67b4a341828987b5119c934fa5da3b5d2f41f7
leehyeonmin <hyunmin2667@gmail.com>
2024-03-21 17:35:53 +0900

    [feat] #187 게시판 api
    Former-commit-id: 97a420817db6bcd90a73a30ba1e6d49a08c417dd

--- back/src/main/java/com/msm/back/board/service/BoardService.java
+++ back/src/main/java/com/msm/back/board/service/BoardService.java
@@ -1,7 +1,3 @@
-package com.msm.back.board.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class BoardService {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:da555b1d9e6ff09f5c481013bfc494131b6050463fe75df12395110369498f09
+size 3802
