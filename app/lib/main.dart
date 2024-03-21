0b622f2e54bc7bd7e78c88128a2319dea9c628b7
김동영 <ehddud0534@naver.com>
2024-03-22 00:34:07 +0900

    [Feat] #243 메세지 전송 확인
    Former-commit-id: 542783135459dd7827ddda126af6aa49f7a44085

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10d520bfd8a6af2cfc496c0c22783fe558dbb3d61cbdcaff96f0c984c15bb3cb
-size 2524
+oid sha256:cd023bca396c50401b2d876e9d77e7f5c4b08cba9dd55509a0e19368286ce14a
+size 5297
