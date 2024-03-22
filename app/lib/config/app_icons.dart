5201a737728dcfacf910d1cee33d3e79b03b0004
황채원 <wondreaming@naver.com>
2024-03-22 19:00:29 +0900

    [Feat] #304 상품탐색 UI
    Former-commit-id: 6af168574174a0bb1f4cf49c6ea08d0f0a58d14e

--- app/lib/config/app_icons.dart
+++ app/lib/config/app_icons.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d67501f220456c0a3c7b3eb7138b3802c9663b2523993194e9d1f90928c3691
-size 826
+oid sha256:4f7421bef70d68cb85ce778ddaf9f196fc93409829ff35faebd318e945c9ab1b
+size 1122
