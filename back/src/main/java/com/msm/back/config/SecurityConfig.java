4c43108cfeec2d1cf51d09f5616e80ac9fbd32e9
송승준 <ssj0187@naver.com>
2024-03-22 14:59:52 +0900

    [Refactor] jwt 인증 설정 변경
    Former-commit-id: f1b27406de458ab2dd79a13b4423a2454e120afd

--- back/src/main/java/com/msm/back/config/SecurityConfig.java
+++ back/src/main/java/com/msm/back/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f45f275b431a32d7e97e7f1c85a2498785bb0bcdb4b27ce2074d52ffb5289bd
-size 3587
+oid sha256:699d4be2c226299921611901346dae2534da9c818c1593f1faaeabc4473473ab
+size 3711
