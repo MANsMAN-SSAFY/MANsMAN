284b3fbc984c74f798db674e1567f97163346456
leehyeonmin <hyunmin2667@gmail.com>
2024-03-22 11:32:47 +0900

    [Feat] #188 게시판 정상작동(아직수정중)
    Former-commit-id: 08c288eda73f1937ba5fc0331844e1b07ee5433f

--- back/src/main/java/com/msm/back/board/controller/BoardController.java
+++ back/src/main/java/com/msm/back/board/controller/BoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd6c37fb712ac438a1f330cbe4f24d6bfd9ab2468ef3c15f33ec5e4cbbe92495
-size 3206
+oid sha256:be9584bc4713b1ecdd50b6116c716135ad397686c71fe9f6790f5e3b0064329e
+size 3222
