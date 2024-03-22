26343810903c88496f04ac2222aee9d59cfd7918
김동영 <ehddud0534@naver.com>
2024-03-22 17:48:19 +0900

    [Feat] #250 알림 API 알림 컴포넌트
    Former-commit-id: acca27ce00592c6f336094012ff39c83a5e434cf

---
+++ app/lib/components/notification/notification.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9470f5eb5466f7fba29991170434fae98a0c844dd0595492d26a0526ec5fd97b
+size 4161
---
+++ app/lib/store/notification/noti.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42a163158651592b181a1c7c7c8b364623a02f9a981eab4389eecef0733a76a3
+size 1199
