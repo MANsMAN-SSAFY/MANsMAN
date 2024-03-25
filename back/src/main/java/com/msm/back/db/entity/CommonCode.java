7f884f91e1a84aa9c42b71fa65e39db60020419e
송승준 <ssj0187@naver.com>
2024-03-25 16:07:53 +0900

    [Refactor] Report Entity 변경 및 로직 수정
    Former-commit-id: 46c50f9472e6dd3170d7a4c255ed6c3ef6c02f5e

---
+++ back/src/main/java/com/msm/back/db/entity/CommonCode.java
@@ -0,0 +1,20 @@
+package com.msm.back.db.entity;
+
+import com.msm.back.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class CommonCode extends BaseEntity {
+
+    // 공통코드
+    @Id
+    private int code;
+
+    // 공통코드명
+    private String codeName;
+
+    // 부모코드
+    private int parentCode;
+
+}
