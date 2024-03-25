9825c2193be903f6fb0b74bc6683fded76db8c6f
김동영 <ehddud0534@naver.com>
2024-03-25 15:14:38 +0900

    [Feat] #251 백그라운드 라우터 이동
    Former-commit-id: 0dd97478a553e7227fa5a9ec78ea5059bdedf464

--- app/lib/components/notification/fcm.dart
+++ app/lib/components/notification/fcm.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f7cdc72420b44892feb27364cf5adfffd88aa6da4329748a702535df341e0a4
-size 2199
+oid sha256:78886f5fc508c0e09b16356982057a8befdb32cd18e1165ef39444de0e1f65d7
+size 2495
