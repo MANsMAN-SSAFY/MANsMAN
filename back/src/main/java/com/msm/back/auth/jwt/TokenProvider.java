7da14dd249620089649044e3258b33c8ae915224
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-03-26 23:57:38 +0900

    [Feat] jwt 토큰 예외 로직 추가
    Former-commit-id: c3547e2d84b87fcab1fbe663bf0afacc0404f87b

--- back/src/main/java/com/msm/back/auth/jwt/TokenProvider.java
+++ back/src/main/java/com/msm/back/auth/jwt/TokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a6181011117cd6d1012a73f1397cc8f2e958c4835651763090a73ee0ecf812c
-size 4927
+oid sha256:0d38bfcdb7e77056815643bb20a8a03ef50fcf74d32ab609cec13026f3fa70e8
+size 5670
---
+++ back/src/main/java/com/msm/back/common/GlobalExceptionHandler.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6db76b50da1f8e77ff515aba2cfe76509dd4eefb0cf7fdf974caa03e38d7ade3
+size 1342
