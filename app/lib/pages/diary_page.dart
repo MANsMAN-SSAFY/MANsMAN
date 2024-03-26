700fbb11a807888b847bbb74b9e59e439d338624
김동영 <ehddud0534@naver.com>
2024-03-26 22:50:34 +0900

    [Feat] #233 피부일지 건강정보
    Former-commit-id: 700b6b3d1ee99676a67f4bdd8486118b1f216628

--- app/lib/pages/diary_page.dart
+++ app/lib/pages/diary_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b80ce60c23e95815e6a534a9b31f4ee57dba497b508a5865302a02c9e0410de
-size 2485
+oid sha256:c9ae264f83bd02ec75b7ca3f6b149b1f544449d9febfeefa34b451ecf07e7941
+size 2602
