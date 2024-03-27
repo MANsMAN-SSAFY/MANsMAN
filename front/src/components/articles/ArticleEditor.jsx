aad00a544ce6347c70f8a576450f96bf3e44959c
박정호 <cuzzzu1318@naver.com>
2024-03-27 11:43:49 +0900

    Revert "Merge remote-tracking branch 'origin/develop' into develop"
    This reverts commit ad6e6aff55c19488a058a8337ff5b07aa176c3e9 [formerly 7334118ed246c417506349d237b798c28f3a375f]
    
    Former-commit-id: b996189efc6a80412e1800eaa7e20629de9547f2

--- front/src/components/articles/ArticleEditor.jsx
+++ front/src/components/articles/ArticleEditor.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:642cce93d151f117fedf647e6b629aaadd8b6ce8fb5ad7a498095338a2f58b47
-size 3392
+oid sha256:b0d9a0319ff461f27653c6a50ff6d2276b8208e8570686a755647cf7ef11ae09
+size 3089
--- front/src/store/user.jsx
+++
@@ -1,8 +0,0 @@
-import create from "zustand";
-
-const useUserStore = create((set) => ({
-  token: "123123",
-  setToken: (item) => set({ token: item }),
-}));
-
-export default useUserStore;
