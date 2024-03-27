f8cd4b4462f8a36577a38d29c01dd09d61d8b20a
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-03-28 03:22:39 +0900

    [Refactor] 재발급 로직 수정
    Former-commit-id: d0ed2a02bc774e2e09482fcb634a9040c437a129

--- back/src/main/java/com/msm/back/db/repository/RefreshTokenRepository.java
+++ back/src/main/java/com/msm/back/db/repository/RefreshTokenRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByValue(String value);
+
     Optional<RefreshToken> findByKey(String key);
 
     void deleteByKey(String key);
