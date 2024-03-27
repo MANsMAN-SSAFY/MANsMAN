ada82f80504123e934b34f6cff7dfe92a8ec06cc
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-03-27 20:05:21 +0900

    [Fix] CORS 설정 변경
    Former-commit-id: ec4f0f6c0e699bc3a2c21ffe9d3bf4df5c122ed5

--- back/src/main/java/com/msm/back/config/SecurityConfig.java
+++ back/src/main/java/com/msm/back/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa89124cb6153bd001b5886488b4b9480f0ed9c2c6790e98f4e0234cdd5d5310
-size 3573
+oid sha256:72055303639ae1df96423ed245634fce10c243626f2444bc521debc789eeb7a3
+size 3627
