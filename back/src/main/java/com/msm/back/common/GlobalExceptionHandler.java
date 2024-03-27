09b253c6b65cc6f52dc40bc3ee6aa5da89788577
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-03-27 22:56:50 +0900

    [Refactor] 알림 토큰 예외 처리 로직 추가
    Former-commit-id: 37c294cf72a1a984b4a019fbc7e77b2963d00b75

--- back/src/main/java/com/msm/back/common/GlobalExceptionHandler.java
+++ back/src/main/java/com/msm/back/common/GlobalExceptionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e86506d64f33d447222035b7d65d9600f8b09ab0ae0d866ed7a74557e2ade549
-size 3007
+oid sha256:49380c499809678e421645de548c1627abd9ddb0cc9efb43a1eef78ae2721e24
+size 3720
