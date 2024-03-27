f903b4a8e2552eb994a202776efbb881c247fe2f
송승준 <ssj0187@naver.com>
2024-03-27 17:59:24 +0900

    [Refactor] 복합키 설정 추가
    Former-commit-id: bf4a2004cfd971dec28e2ab580f7a5b931d50bd2

--- back/src/main/java/com/msm/back/db/entity/MyProduct.java
+++ back/src/main/java/com/msm/back/db/entity/MyProduct.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8694c66b47e4d02c5788a54212e30fae5abefec94d538502fb8e14a1e05f5e69
-size 556
+oid sha256:1e4cb9ce6d1a9317579034691d8e9fc9a8d54344d35c3d9be060f19befa9d2ea
+size 566
