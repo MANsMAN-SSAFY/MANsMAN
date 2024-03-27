3c4ae2e343ccbab8e77f72f3651eb1e15d5fad1d
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-03-27 22:37:41 +0900

    [Refactor] 토큰 재발급 Response 수정
    Former-commit-id: 47f200ccba926491c2bd4c75f781616e27966ad6

--- back/src/main/java/com/msm/back/auth/service/AuthService.java
+++ back/src/main/java/com/msm/back/auth/service/AuthService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b49d1b2786ff36277cc150d6e2fd0b8e54240084d27392ea6ea12c284767cbf8
-size 9052
+oid sha256:694246e04aab3db896a44f3572fc438d4bd7cdb74087f0133d4872c90b72d486
+size 9277
