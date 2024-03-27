d55ff26993601a140d6ca7ad93bbf08e8bf4ec6c
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-03-27 22:16:45 +0900

    [Refactor] 평점 타입 변경(int->double)
    Former-commit-id: b93f95542cf4589575ab5c33f52062dfdcd37288

--- back/src/main/java/com/msm/back/member/controller/MemberController.java
+++ back/src/main/java/com/msm/back/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bda63f66df673197e280b517979769a6228cbda9b6789cc175944c9cdbd7798
-size 5153
+oid sha256:75ec0413bdd54558692e6a427748ec334bb34a4df343d692535dc3ceeb9e5fe4
+size 5067
