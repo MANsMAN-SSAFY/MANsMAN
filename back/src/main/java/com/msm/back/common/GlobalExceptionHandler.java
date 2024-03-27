676c804af4a1895648c809a1c9a3c5c677eb205b
송승준 <ssj0187@naver.com>
2024-03-27 15:12:11 +0900

    [Refactor] 로그인 에러 처리 추가
    Former-commit-id: c10d795ece3f628611d68b20870288d7ec69ce84

--- back/src/main/java/com/msm/back/common/GlobalExceptionHandler.java
+++ back/src/main/java/com/msm/back/common/GlobalExceptionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0f645fee4d59796de6c902648a463ee64c3683a5ef86e79b2589f140d4d1a09
-size 2607
+oid sha256:e86506d64f33d447222035b7d65d9600f8b09ab0ae0d866ed7a74557e2ade549
+size 3007
