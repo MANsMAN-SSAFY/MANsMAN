46d964f719abf848d9fa4391418334625e00c525
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-03-27 23:08:13 +0900

    [Fix] Swagger 접근 허용 로직 추가
    Former-commit-id: 85837fb3ef2ff0d0299a4651c892678ef4ba106d

--- back/src/main/java/com/msm/back/config/SecurityConfig.java
+++ back/src/main/java/com/msm/back/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa89124cb6153bd001b5886488b4b9480f0ed9c2c6790e98f4e0234cdd5d5310
-size 3573
+oid sha256:9ddf5cb58c1507ddc77533416630e5313991cb8a502d4f924918fd64f1783ca8
+size 3533
