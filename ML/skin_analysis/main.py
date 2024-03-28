2825a213eaacaa4fd2768e936a5f7692ad0c9369
박정호 <cuzzzu1318@naver.com>
2024-03-28 17:23:37 +0900

    [Feat] #327 유사한 화장품 fastapi
    Former-commit-id: df51acaac53894da41c24809195e877e95a16127

--- ML/skin_analysis/main.py
+++ ML/skin_analysis/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ce937884ec91a221be636bf2f2c1cc4cd0aa175b4fbe1c1a0497d127b5d9092
-size 9422
+oid sha256:225d89a6fce9de95e3f2a730f2504d5db1ba227bbd48a337668b2b74adbbc349
+size 10031
--- back/src/main/java/com/msm/back/product/controller/ProductController.java
+++ back/src/main/java/com/msm/back/product/controller/ProductController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4a23f47ee6456b08a30054ae13a7427d3248538a7bff554e29ac876670ad056
-size 3357
+oid sha256:ca090cba70bc119bfb414c6589f131219a4a829bcb37dcced77661b3f93b42ac
+size 3814
