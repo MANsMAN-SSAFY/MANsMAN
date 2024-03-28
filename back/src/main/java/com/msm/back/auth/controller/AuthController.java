17a7af9d3618b8fc09dee5901be421313bdd4681
송승준 <ssj0187@naver.com>
2024-03-28 15:18:31 +0900

    [Refactor] 토큰 재발급 로직 수정
    Former-commit-id: 291a49add44a50450c8f111d7e332f7fafa74a74

--- back/src/main/java/com/msm/back/auth/controller/AuthController.java
+++ back/src/main/java/com/msm/back/auth/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:564f6a910a3493a359d58ebae8cff9aab1736fa988db1269ab92b3daaa5e29c8
-size 1498
+oid sha256:8d3d4245c71a5bd9c5c848e41e748847dbebf61661e25518e7dda6ebc4d8c162
+size 1505
