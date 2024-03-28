61af69e6299b2c83a58721ae5f899f2a03c7775d
박정호 <cuzzzu1318@naver.com>
2024-03-28 17:23:37 +0900

    [Feat] #327 유사한 화장품 fastapi
    Former-commit-id: 63ba20015b88737a60b52e65a75844e230dc405d

--- back/src/main/java/com/msm/back/db/repository/ProductCustomImpl.java
+++ back/src/main/java/com/msm/back/db/repository/ProductCustomImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dce454cb124ca705439de6f6402cf79518cda7474f258c5c682416f1d8c46592
-size 3131
+oid sha256:c7808856a30c761fee8d77cb1e373120b9a11fc4411b1fac745d9f0222c6e029
+size 3444
