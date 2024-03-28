d0646bc279c57a8cb921a99f0460855173bcfbbd
김동영 <ehddud0534@naver.com>
2024-03-28 15:30:38 +0900

    [Feat] #308 게시판 수정
    Former-commit-id: d764d36859b8a98fe16aefe06d3a54d4a48dd6c4

---
+++ app/lib/components/community/boards_list.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:64d8284d892f2523b1d1722e03cb63602789906bf7225dd874fd0b69c89b730a
+size 2522
---
+++ app/lib/model/articles/articles.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:79709ae1624df1cb714d36da659ff5610d6e979cc323a2e55c7f4f809359bd45
+size 2641
