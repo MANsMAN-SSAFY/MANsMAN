f12d47ab6d508fe13232d1be25d64bb6c5bdf599
leehyeonmin <hyunmin2667@gmail.com>
2024-03-29 10:33:04 +0900

    [Feat] #246 댓글 및 게시물스크랩
    Former-commit-id: 4ae1a22f8688d66531c4d9a739bae1976239f7b7

--- back/src/main/java/com/msm/back/db/repository/ScrapRepository.java
+++ back/src/main/java/com/msm/back/db/repository/ScrapRepository.java
@@ -1,5 +1,7 @@
 package com.msm.back.db.repository;
 
+import com.msm.back.db.entity.Board;
+import com.msm.back.db.entity.Member;
 import com.msm.back.db.entity.Scrap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +9,6 @@ import java.util.List;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
     List<Scrap> findByMemberId(Long memberId);
+    boolean existsByBoardAndMember(Board board, Member member);
+    Scrap findByBoardAndMember(Board board, Member member);
 }
