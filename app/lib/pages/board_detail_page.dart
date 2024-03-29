28fc98a35e3b8554f0bf31b752a200177f6cc80e
김동영 <ehddud0534@naver.com>
2024-03-29 14:15:36 +0900

    [Feat] #308 게시판 댓글 조회
    Former-commit-id: 1b408b3768680b0fb2ed6b48f833c262d15863a5

--- app/lib/pages/board_detail_page.dart
+++ app/lib/pages/board_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa7b83e6e4dcdbda32750ae0510cba93e162ed9d251fb5759df52302a99b0291
-size 16026
+oid sha256:8beab06504b8cd6ba20074161e227b572ce15efb79e7cd28b5bac4b1353d7687
+size 18562
