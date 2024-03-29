022fdacfebbdd8c1eb1aa5c4740912ceca0ce1db
박정호 <cuzzzu1318@naver.com>
2024-03-29 12:06:50 +0900

    [Fix] 화장품 url 잘못된 값 수정
    Former-commit-id: cce8e4f18cd5bff215a007589ba89eed7cba8291

--- back/src/main/java/com/msm/back/product/dto/ProductListDto.java
+++ back/src/main/java/com/msm/back/product/dto/ProductListDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e97bdd0f1e9387245237ed5ccdcaf24a07697e8c91953e2d7eb95788e03dc4c
-size 1385
+oid sha256:81d4ec47abb7d6e416e8ee9f82c6c0790c104444d7bf2e9407c9551d1cd827a3
+size 1387
