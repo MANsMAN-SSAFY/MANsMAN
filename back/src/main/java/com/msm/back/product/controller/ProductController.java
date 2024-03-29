994a5a81960878b4b0f7976cc0011811c1eacb8e
박정호 <cuzzzu1318@naver.com>
2024-03-29 15:33:47 +0900

    [!HOTFIX] 화장품 인덱스 에러 수정
    조회할 값 없을 때 뜨는 에러 수정
    
    
    Former-commit-id: f3e2eed36a4c44cb620c81573215e762fef08660

--- back/src/main/java/com/msm/back/product/controller/ProductController.java
+++ back/src/main/java/com/msm/back/product/controller/ProductController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30c192b9bb5469194d89307259abdfe26129c09ec4d0877df2f658c1437a4789
-size 3991
+oid sha256:c989ee5b5aea83feb65ddb82e9b35707a95b5ae1ed76dc989a3338c9e6e7a0db
+size 3990
