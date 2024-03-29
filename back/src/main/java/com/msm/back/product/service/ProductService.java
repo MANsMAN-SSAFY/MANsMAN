474eb6fb9dc7de7364d15f229e49267dde2ab9e6
박정호 <cuzzzu1318@naver.com>
2024-03-29 11:36:06 +0900

    [Fix] 즐겨찾기 쿼리 횟수 줄임
    Former-commit-id: f8be3bcba0b13a34c870b594361b4f07c6e95926

--- back/src/main/java/com/msm/back/product/service/ProductService.java
+++ back/src/main/java/com/msm/back/product/service/ProductService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fbd1a3fc3b0ad4807172bbc9fb7688ce036abbaad3ac5c2229ad5c55a17f267
-size 6049
+oid sha256:5b8b101624bba36fafa4e6ac9679be0f4caebc18ece8db320e59e2c236501e65
+size 5716
