8ea150f31a8218cd096ba7849b934bff47b176d4
김동영 <ehddud0534@naver.com>
2024-03-29 21:05:36 +0900

    [Feat] #232 피부 분석
    Former-commit-id: d6611200cc87aec457a12846db5aece021dc7717

--- app/lib/components/notification/notification.dart
+++ app/lib/components/notification/notification.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:447c9360c8d0c57ace52fda4ee1b1853b09d12248e34517edb394c9c58eb0a79
-size 8049
+oid sha256:4c352ee962787a173532b75f32446e8c5bd3490727dbb4e9d0510b10d6830fcf
+size 8298
