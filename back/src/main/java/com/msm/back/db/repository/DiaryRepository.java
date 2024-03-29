a20afebc9ee0ebc55d75730ca45a05e853a2528e
crcho5133 <140131170+crcho5133@users.noreply.github.com>
2024-03-29 21:37:41 +0900

    [Feat] #305 피부분석 리포트 - 최근 n개 가져오기
    Former-commit-id: 21508a68275a93813b1846279d7d3905ad8da603

--- back/src/main/java/com/msm/back/db/repository/DiaryRepository.java
+++ back/src/main/java/com/msm/back/db/repository/DiaryRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efc29b140e6bafd53155462df599865913058593076a79e4303dd2023b56c747
-size 886
+oid sha256:71128adddff4f1e19f7a0a176cb31bad05d2112d08528f53615f8ed8fbff2a3a
+size 1110
