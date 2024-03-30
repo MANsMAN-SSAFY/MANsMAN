b8ae9700e074b936825500d09de77b78082b7c44
박정호 <cuzzzu1318@naver.com>
2024-03-30 15:04:10 +0900

    [!HOTFIX] Redis 설정 수정
    Former-commit-id: 8546ff6e9c87a342132c2c5ee931a172b5727be7

--- back/src/main/java/com/msm/back/config/RedisConfig.java
+++ back/src/main/java/com/msm/back/config/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:132a8ed6d24b90f216c9292312517c77e79ed1b1163a304987b68e847270e308
-size 857
+oid sha256:2310bd77e92cd54da14f2006f9d5a7a7324ba7b5e115d8077bab8e0da7a33ba2
+size 1663
