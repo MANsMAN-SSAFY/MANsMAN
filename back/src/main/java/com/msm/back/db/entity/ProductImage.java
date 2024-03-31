bdd3e52dbb4e77ca8dc224aa92f95c86163dd600
박정호 <cuzzzu1318@naver.com>
2024-03-31 14:20:30 +0900

    [Fix] 상품 이미지 에러 수정
    Former-commit-id: 4089e9e584e2ca93bce9ad0ae23d801437c872a4

--- back/src/main/java/com/msm/back/db/entity/ProductImage.java
+++ back/src/main/java/com/msm/back/db/entity/ProductImage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:683dfbc02fc37d9d07356d7b2cf09f794366cf8b95745debe8d75d6e17e3901b
-size 697
+oid sha256:396ad3da7726611f1f9bb5dc517e1d2fe43f516bda5459f7b1cf7f7189c3809c
+size 672
--- back/src/main/java/com/msm/back/product/service/ProductService.java
+++ back/src/main/java/com/msm/back/product/service/ProductService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8f04472ef54dce22aeeacaa91be1aff89ae58bec793970fac93b7998d1ee43a
-size 5876
+oid sha256:dbd8e2623daa63c949d0ec19bf01291b7299d941a7d3b18634ee5ca57b72dc6a
+size 6226
