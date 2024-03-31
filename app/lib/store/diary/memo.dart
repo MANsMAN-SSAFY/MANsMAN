5c24d79d81179b453c15dfdf1f6f6e7f519b2f2c
김동영 <ehddud0534@naver.com>
2024-03-31 23:46:08 +0900

    [Feat] 알림 수정, 게시글 썸네일 수정
    Former-commit-id: 0bb9be4ab4ce26bdfbcd23986d3959a8f0e075ff

--- app/lib/store/diary/memo.dart
+++ app/lib/store/diary/memo.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:797b9794016b4ee538bdaf5e0a77b1e2b6029f1669d81f53a6bf1b3297eccd33
-size 683
+oid sha256:f11697b3ad85d48fcb82d34c8ab4eba8dcbdb94111075e467243fb465dc12bd9
+size 775
