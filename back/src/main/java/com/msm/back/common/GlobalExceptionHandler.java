cd0b06b74ba5a76e0f6c58a56b682d077de22a3a
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-03-31 13:21:27 +0900

    [Refactor] 에러 response 수정
    Former-commit-id: c18daa2a7fdb188437344380af587b816285c1a6

--- back/src/main/java/com/msm/back/common/GlobalExceptionHandler.java
+++ back/src/main/java/com/msm/back/common/GlobalExceptionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:224d2d2c6ba50bb8c432625ee75e8666237db555e9e22e0321b1b69f704d3358
-size 7192
+oid sha256:5fe860c6b95210580068e958e1a3631bdb3c57338c593fa7db73db3333a402af
+size 4744
