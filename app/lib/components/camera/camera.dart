d38f46b97b2922539936a30ccb29c5438f938781
김동영 <ehddud0534@naver.com>
2024-03-31 19:38:24 +0900

    [Feat] #232 일지 메모
    Former-commit-id: 43b0405c1603d7c5d930108fae8b49552ac04890

--- app/lib/components/camera/camera.dart
+++ app/lib/components/camera/camera.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:448ae11d20f2230bf3caa9950a95e73fa6f5f2395cc23e9e4aa4df6c0bd98f57
-size 5009
+oid sha256:6d8bad737a819d589aa378828d713cbd3187caae16ae54e7cf6f3c86f49878d3
+size 5042
