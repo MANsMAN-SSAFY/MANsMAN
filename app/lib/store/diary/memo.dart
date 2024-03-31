8a4f2db0f7af6744eba9a17c0871f6487742e0d4
김동영 <ehddud0534@naver.com>
2024-03-31 19:38:24 +0900

    [Feat] #232 일지 메모
    Former-commit-id: e14301d5ff1a9a1fbcac2f9cb4fcd8068d56027d

--- app/lib/store/diary/memo.dart
+++ app/lib/store/diary/memo.dart
@@ -1,11 +1,3 @@
-import 'package:flutter/material.dart';
-
-class Memo extends ChangeNotifier {
-  String _memo = '오늘의 한줄을 추가하세요.';
-  String get memo => _memo;
-
-  void setMemo(String memo) {
-    _memo = memo;
-    notifyListeners();
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:797b9794016b4ee538bdaf5e0a77b1e2b6029f1669d81f53a6bf1b3297eccd33
+size 683
