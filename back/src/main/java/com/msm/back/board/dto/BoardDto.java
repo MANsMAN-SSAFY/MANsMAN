f0d3c6823633df02a3b57124b1fe513cf5cdc606
박정호 <cuzzzu1318@naver.com>
2024-03-31 23:16:39 +0900

    [Fix] 게시글 리스트 좋아요/스크랩 여부 조회
    Former-commit-id: 4e66de3e1e967cf002a9968b1eecfb5015215d25

--- back/src/main/java/com/msm/back/board/dto/BoardDto.java
+++ back/src/main/java/com/msm/back/board/dto/BoardDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54c2fcacc372bad9f0f1017fc319830081af1fe4603416855c55e807b7e5dd6f
-size 1239
+oid sha256:36543515e13976cfa54e18e2711ffbf315b1b4a53ccd15e65c56829ac2033115
+size 1391
