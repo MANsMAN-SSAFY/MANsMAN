69a25866247d22db7a5e07fa46d0f7fda372ddd7
박정호 <cuzzzu1318@naver.com>
2024-04-01 12:36:36 +0900

    [Fix] 내 상품 평정 상품에 반영
    Former-commit-id: 458aad47dada64ec21f7f74cc3b6c0e3c5b48e90

--- back/src/main/java/com/msm/back/product/dto/ProductDetailResponseDto.java
+++ back/src/main/java/com/msm/back/product/dto/ProductDetailResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5da00ad21a7a9cc8437e49908b1a9fc7125adf0165056389d8f4d6050cb10db7
-size 1191
+oid sha256:52d0e9e998ea4ae9b9d109a5a3cc190cd270ae37d8585257416b36664fbcc887
+size 1192
