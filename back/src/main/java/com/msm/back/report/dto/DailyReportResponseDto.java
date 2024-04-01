1038e772778198de884c8f5d17e10eb148f64032
crcho5133 <140131170+crcho5133@users.noreply.github.com>
2024-04-01 17:53:48 +0900

    [Feat] #358 피부일기를 피부 리포트에 결합
    Former-commit-id: d9938959499f5bd590998fc631470a9353e45d09

--- back/src/main/java/com/msm/back/report/dto/DailyReportResponseDto.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:955215afe4105a91bcd3240dee14acd34d701dfead1d414d59d705dac0fae155
-size 611
--- back/src/main/java/com/msm/back/tag/dto/TagResponseDto.java
+++ back/src/main/java/com/msm/back/tag/dto/TagResponseDto.java
@@ -11,6 +11,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class TagResponseDto {
     private Long tagId; // 태그의 id
-    private Long diaryId; // 태그가 속한 피부 일기의 id
+    private Long reportId; // 태그가 속한 피부 분석 리포트의 id
     private String content; // 태그의 내용
 }
