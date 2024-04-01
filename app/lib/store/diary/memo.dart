40a8c033d7d545d4fbb769b471f957f72cac8e29
김동영 <ehddud0534@naver.com>
2024-04-02 08:57:28 +0900

    [Feat] 일지 페이지 메모 추가
    Former-commit-id: 935c88de3f8b6ae5ea79f3974f58dfb1d2df9e6f

--- app/lib/store/diary/memo.dart
+++ app/lib/store/diary/memo.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56287aeec34025d67b05ce915293e03eebd0f186f273cd9e158e8b6e6edb9173
-size 1429
+oid sha256:b9a207380c7e1bd1a4750347456379340ce436df3b78d7d5a0634bc752e3f689
+size 1169
