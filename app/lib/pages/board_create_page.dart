6393d7bbdeb53cea72acfa78a2f76f478e19d657
김동영 <ehddud0534@naver.com>
2024-04-01 10:51:07 +0900

    [Feat] 게시판
    Former-commit-id: f9337314b9b3fff917c733a5b7e52c622625e218

--- app/lib/pages/board_create_page.dart
+++ app/lib/pages/board_create_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9770ee3501b3ff80b793cce976c53a09d6331790b0e0e05f09e2ad6c9bffbee
-size 6606
+oid sha256:23188fbd5f82c9637361f79973c57b73d9c6124ddc75ffa7eaf5f8f11ed18d87
+size 6649
