6ec900a0ac6c4f1d7a32a7b6ac35ecf332bc11ec
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-04-02 00:36:57 +0900

    [Refactor] 게시판 상세 정보 수정
    Former-commit-id: d7b80dd36702734b8aaf18173aca264c4c7824f4

--- app/lib/components/community/comment_item.dart
+++ app/lib/components/community/comment_item.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db10648d5bea341f745fbc2db82350ba49ce6902eefc0d8600a437b3ed758a86
-size 7411
+oid sha256:5f639ef04aa1d050abac7cdb1f9f9da5edeade3a06318a91a2e0d2f43b32bff0
+size 12820
