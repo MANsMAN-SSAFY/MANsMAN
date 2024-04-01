0e2225a0e9f916254d0ea86c2aec0364422e6358
crcho5133 <140131170+crcho5133@users.noreply.github.com>
2024-04-02 00:37:32 +0900

    [Feat] 내가 사용한 화장품 등록하여 평점을 남길 수 있음
    Former-commit-id: 1980720c2f7f5f11553e0ad3f9c1f8ddbbd88af7

--- back/src/main/java/com/msm/back/db/entity/MyProduct.java
+++ back/src/main/java/com/msm/back/db/entity/MyProduct.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72fb5aea6db1e4fab6071fe66fd7eaf1dba9f49eb018bb54d00d431deca3a0f2
-size 600
+oid sha256:d638584a27ce454a8374c86a9d8d39a822bbf4bdd8fa48e3e2bc5c82b6aac704
+size 1007
