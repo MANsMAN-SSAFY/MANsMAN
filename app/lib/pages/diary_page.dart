ee0f41be53f3855deebce426601a562f44f70dad
김동영 <ehddud0534@naver.com>
2024-04-02 14:12:11 +0900

    [Fix] 일지 페이지 index fix
    Former-commit-id: 20bd897845c79f8955cf1ae40cc84eb2c347e5b4

--- app/lib/pages/diary_page.dart
+++ app/lib/pages/diary_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38ff97598116c1b348c9b14b3141141bf54a3388e58dcb94e95207cfc4b10ed9
-size 38141
+oid sha256:c18d54fde2d82a53e9d66876e09260c40277f24a9afb23d3a7aff5e1207cbb23
+size 39125
