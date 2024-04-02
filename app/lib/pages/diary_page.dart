3882c7a9493c3f133ac9770b757b802ae3db9434
김동영 <ehddud0534@naver.com>
2024-04-02 23:57:13 +0900

    [Fix] 일지페이지 데이터 변경 반영 수정
    Former-commit-id: bc85f034e723cc32324fafd8b22d7b1048f64d49

--- app/lib/pages/diary_page.dart
+++ app/lib/pages/diary_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:909d2ca0148e9c8f3519cfc2bac46eb376cae36885a0f2fb3ce842a206a8559b
-size 45198
+oid sha256:0c659ba7128143f69fb4b500df5023e4cf264ae54e776cf6f655f82eb6996e31
+size 43912
--- app/lib/store/diary/diary_store.dart
+++ app/lib/store/diary/diary_store.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db36b7377cf31d8ea6fac9f95d8d38eb2dc4627babf7f7b24a1bc5f77168696c
-size 5208
+oid sha256:d41fef41b8e77dcba0f0c92562b96c2993e39f87f5c0b084b1137a3eb6c0e381
+size 5286
