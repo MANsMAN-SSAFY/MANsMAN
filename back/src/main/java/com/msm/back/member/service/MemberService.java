de246842836f89b7a8d013a2104c44327ca222b8
송승준 <ssj0187@naver.com>
2024-04-02 15:19:24 +0900

    [Fix] DB 수정
    Former-commit-id: 557101eaf12618fbc8cfae0330785055473495f8

--- back/src/main/java/com/msm/back/member/service/MemberService.java
+++ back/src/main/java/com/msm/back/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0716af08de0015de9bd3199ffcfa2a32290fd03ddcbf0fde90706e46faa5d9c
-size 12261
+oid sha256:aa74ce51b873b4cdad8678dc9f07defb87c2193a95418b914d465bef40ce2653
+size 12262
