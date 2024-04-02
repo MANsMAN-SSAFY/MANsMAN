ed492f036645d7cb5aa51cbf0bd82a41a41a72c1
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-04-02 22:52:07 +0900

    [Refactor] myproduct 상태 관리 로직 수정
    Former-commit-id: d35a7e6e7d2d75f2a0462fabea332624e718e4b4

--- app/lib/pages/cosmetics/review_page.dart
+++ app/lib/pages/cosmetics/review_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aaa34004797f3baa2dfdeeb2f5510151fa6a72da3e23794b8360b6b3f8f2e984
-size 15531
+oid sha256:a0653a0c4219cf1c5ad53a50246b9c90310b9f66304fcbc6c4384caccec29471
+size 14998
