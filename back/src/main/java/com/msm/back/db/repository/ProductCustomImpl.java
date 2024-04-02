2b6ab5db4d6d7f89ccf234542624e32f67fa4779
박정호 <cuzzzu1318@naver.com>
2024-04-02 15:27:00 +0900

    [Fix] 화장품 분류
    Former-commit-id: bcc9e5c3c3ed461919207c921828cde33bb6adf2

--- back/src/main/java/com/msm/back/db/repository/ProductCustomImpl.java
+++ back/src/main/java/com/msm/back/db/repository/ProductCustomImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:392b1ef295fdf2fa4567956634e36ddd9d83974befdb7c738233a91ece8803a1
-size 6406
+oid sha256:81f37bae02c58aef9f87abe0a527ad10359f86fb3bc217267ff1150aafa1aea2
+size 6466
