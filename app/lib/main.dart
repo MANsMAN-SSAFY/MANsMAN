71b56a42cb59705401c52bbd38d38fdaf05407c6
송승준 <ssj0187@naver.com>
2024-04-02 14:19:39 +0900

    [Refactor] 생성일시 포맷팅 변경
    Former-commit-id: 2db38c4f6f7a31cf391992516e9938c870e74b04

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a18a3d629df48311abc9b20eb1303139b2afa133202d62746c1327d5cb711175
-size 6689
+oid sha256:e3380438bc215cca41205408e930b70afd0cdb43ccffc4da5bbc67fe2fbf2183
+size 6763
