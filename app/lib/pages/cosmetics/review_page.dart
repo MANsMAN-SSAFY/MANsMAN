a3d6fba3f5f55e0324b96f4c8761e75c55e6c5a4
황채원 <wondreaming@naver.com>
2024-04-02 17:30:29 +0900

    [Feat] #357 마스크api 연결
    Former-commit-id: 88259039c8131573ee1b112fb035db3eedc4b716

--- app/lib/pages/cosmetics/review_page.dart
+++ app/lib/pages/cosmetics/review_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:639a89cc7823640e25bb54ebc47851b28d6dafe0aa3faae82f56d6d2fd0a34a6
-size 15588
+oid sha256:aaa34004797f3baa2dfdeeb2f5510151fa6a72da3e23794b8360b6b3f8f2e984
+size 15531
