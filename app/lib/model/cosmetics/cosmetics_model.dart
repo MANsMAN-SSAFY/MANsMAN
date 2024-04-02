88f207c0b2304536ab47bdef2d250769267b535a
송승준 <ssj0187@naver.com>
2024-04-02 17:40:00 +0900

    [Feat] 마스크팩 조회 기능 구현
    Former-commit-id: d9736b514ed1a5fe0ec5de423c29a64cd253daa7

--- app/lib/model/cosmetics/cosmetics_model.dart
+++ app/lib/model/cosmetics/cosmetics_model.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4844f0de08455555796f22825d1195a0ff8beed25d101808d8bb8ccc075e2ac8
-size 1002
+oid sha256:227a3a26b610ccc6354ced46788a2974604fc9b78a9907d3af1e3eb8a8454f45
+size 1217
