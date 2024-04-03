a979a88b72898f97fee67cc26cd2d1a89bc3bcc0
김동영 <ehddud0534@naver.com>
2024-04-04 01:14:45 +0900

    [Design] 일지 페이지 건강정보 디자인 수정
    Former-commit-id: 97e3f1d9c74b50112d8d1ef18e2d82b90784f358

--- app/lib/components/health_info.dart
+++ app/lib/components/health_info.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88dfc1dd1981bb97a5241a92b6f0834bb06eb506b58445dbce6d76d2a2a1f150
-size 12443
+oid sha256:5f6adf4d0e419df7be474e4b4082141bf408d55cbbd9feae656a40f45b06e0fa
+size 14691
--- app/lib/pages/board_detail_page.dart
+++ app/lib/pages/board_detail_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f45c911adbbd9d1de3650278b6a9348adeeb97f3fbe847fb273c2551b7623b3
-size 30589
+oid sha256:ee568723feb90cd35442525b3977df15c5318b0c06a90bccf52f84230b8bad58
+size 30995
