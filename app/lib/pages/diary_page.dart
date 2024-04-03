f87e905fb6bc3cb0679a8c095b69770462ff8911
김동영 <ehddud0534@naver.com>
2024-04-03 17:40:29 +0900

    [Fix] 초기페이지 수정
    Former-commit-id: 0a0ad685812180c29dff71cd79b0d69d8dac20fe

--- app/lib/pages/diary_page.dart
+++ app/lib/pages/diary_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9050d5296837d3dcacaf1ac7258d31f639edd723dfb1ea70c5e4af2d003414d3
-size 44720
+oid sha256:fd15003a4e2ad1a360aa158967b4ade8d09b2deb9d30e167290dd66784ec5eaa
+size 44957
