cf452e67aec3ac681c161425434f3e33ee58526b
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-04-03 12:38:12 +0900

    [Feat] #355 피부일지
    Former-commit-id: 6cd1935fb23ca1989043594812af3534362edcee

--- app/lib/pages/cosmetics/cosmetics_page.dart
+++ app/lib/pages/cosmetics/cosmetics_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5e122bf5674f142b3ec0c07e87e1bb309ea59cc762d47f7dd51dd8a86ec6c01
-size 14913
+oid sha256:6106d0271f33ac9d5f176603af794328e8d15a84c160314b1f183f50a9116462
+size 15919
--- app/lib/pages/login/signup_page.dart
+++ app/lib/pages/login/signup_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b0d84b9051751527e424a19c972d5a6b86b2345ea98fe8956a246e6b4a900b1
-size 12299
+oid sha256:8940f1aaa6759ef4babaab1787cab504a1aa7db9e301ae27d6872ff868401665
+size 11853
