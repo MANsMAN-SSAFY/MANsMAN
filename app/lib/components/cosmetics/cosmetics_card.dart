dedaf21bf71ead7111e69f9749bd73a238f232c2
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-04-03 21:29:10 +0900

    [Fix] 화장품 수정
    Former-commit-id: e291e31227f7875ce12cc15bc10e4ec574ca772e

--- app/lib/components/cosmetics/cosmetics_card.dart
+++ app/lib/components/cosmetics/cosmetics_card.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60b1a41cc701243e391d8cb023ba65a8dd72ae4b7d0b01ff0995d777184c9a52
+oid sha256:4af43152b6764727e902bdb792944da104c6eaef572c90299c22e2b5cd074bf6
 size 8608
