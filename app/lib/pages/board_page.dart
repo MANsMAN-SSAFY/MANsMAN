189bf993b9eb7ca99acfbe6d4c84a6f2eb22168d
김동영 <ehddud0534@naver.com>
2024-04-04 03:26:14 +0900

    [Refactor] 커뮤니티 페이지 알림 버튼 추가
    Former-commit-id: 75e415340a968ff2107d9dc5b49e48136c9da8be

--- app/lib/pages/board_page.dart
+++ app/lib/pages/board_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b367a787e73280c4b3ee55afe4df974a4205055b0498c81bb0d25d3b7256947
-size 4291
+oid sha256:2d72f6442d3e36c6ca5d8abe9d7209f584d90c850bd222bf1f14c770a8c0fef8
+size 6022
