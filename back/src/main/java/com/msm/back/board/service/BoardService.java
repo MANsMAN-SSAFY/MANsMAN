506eea528fd1ac7e6bd959955d2a8aff64f6bb2b
송승준 <ssj0187@naver.com>
2024-04-04 03:37:52 +0900

    [Fix] 알림 로직 수정2
    Former-commit-id: beb5a5c00c775b739d620be9a9b6edcf6f05a0cb

--- back/src/main/java/com/msm/back/board/service/BoardService.java
+++ back/src/main/java/com/msm/back/board/service/BoardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:955e878e4f2e18ad212fbb6fa86f3e3724768d9a45ee8cb44f7cddb64a522dca
-size 11127
+oid sha256:f5223be0818358bd463703c040665b6456475ef304ca6d2be2c3726f8c1e9c32
+size 10748
