ae5b18646698ed3f1a9642ef072f718a0cf528e7
김동영 <ehddud0534@naver.com>
2024-04-03 21:29:36 +0900

    [Fix] 다이어리 페이지 에러 수정
    Former-commit-id: 12a3ce569fbec5f3c669cd12c590564b63d7d3df

--- app/lib/pages/diary_page.dart
+++ app/lib/pages/diary_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd15003a4e2ad1a360aa158967b4ade8d09b2deb9d30e167290dd66784ec5eaa
-size 44957
+oid sha256:7c6a11eb8c756b62c40521775602a58e1525a028afee1f3fee2fc71c976141d2
+size 45355
