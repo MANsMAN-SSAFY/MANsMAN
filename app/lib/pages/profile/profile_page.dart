917e017b94b225be81edabf256ee39b15ab31587
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-04-04 09:18:45 +0900

    fix/FE-otherprofile
    Former-commit-id: 757b235eea290bc8d70bbd81280063d3e80bcfb3

--- app/lib/pages/profile/profile_page.dart
+++ app/lib/pages/profile/profile_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc169a0096970d5b64bbfefba3755d4ff926bf6f55a0a61535d5e31650761440
+oid sha256:646e74c29e87de70e6979ed3f2ebef3d05e294da2de507d9da65283cc921d445
 size 10235
